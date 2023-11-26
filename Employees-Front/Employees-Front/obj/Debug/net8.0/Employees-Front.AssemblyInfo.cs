91b2e0523a1a5f1a9e7a79a67cfdad61a3d24bf0
Mariángela Navarro <mariangelanavarro10@gmail.com>
2023-11-26 15:47:25 -0600

    Solution bug in home

--- Employees-Front/Employees-Front/obj/Debug/net8.0/Employees-Front.AssemblyInfo.cs
+++ Employees-Front/Employees-Front/obj/Debug/net8.0/Employees-Front.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employees-Front")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9865c08aeb5ac4d3b7e77fb134f9c259d15c364c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a86debe0479ff16bd6f26ee9a967c7b51332cb62")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employees-Front")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employees-Front")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
