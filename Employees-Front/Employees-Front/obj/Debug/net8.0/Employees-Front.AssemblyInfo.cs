da46cac3d2b269f9774c905ef6c46fda7981a091
Mariángela Navarro <mariangelanavarro10@gmail.com>
2023-11-28 21:17:50 -0600

    bug solution to present error in form

--- Employees-Front/Employees-Front/obj/Debug/net8.0/Employees-Front.AssemblyInfo.cs
+++ Employees-Front/Employees-Front/obj/Debug/net8.0/Employees-Front.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employees-Front")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6589d3dec9a4efda9c81613b9b6ed15c56efa465")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a1a86eebe32b4324d8e4deb9f801f621186e33d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employees-Front")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employees-Front")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
